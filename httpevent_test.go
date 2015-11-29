1b351be713bfef6ff567eff9f9fda56a677cac6a
Christian Galsterer <christian.galsterer@gmx.de>
2015-11-29 18:21:13 +0100

    - First tests for output events

---
+++ httpevent_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"testing"
+	"time"
+	"github.com/elastic/libbeat/common"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHttpEventToMapStr(t *testing.T) {
+	now := time.Now()
+	fields := make(map[string]string)
+	fields["field1"] = "value1"
+	fields["field2"] = "value2"
+
+	event := HttpEvent{}
+	event.Fields = fields
+	event.DocumentType = "test"
+	event.ReadTime = now
+	mapStr := event.ToMapStr()
+	_, fieldsExist := mapStr["fields"]
+	assert.True(t, fieldsExist)
+	assert.Equal(t, "test", mapStr["type"])
+	assert.Equal(t, common.Time(now), mapStr["@timestamp"])
+}
+
+func TestHttpEventToMapStrWIthEmptyFields(t *testing.T) {
+	event := HttpEvent{}
+	mapStr := event.ToMapStr()
+	_, fieldsExist := mapStr["fields"]
+	assert.False(t, fieldsExist)
+}
