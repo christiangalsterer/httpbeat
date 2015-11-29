713343168bc98dd6fcb9d7a9e7eb588a6af1f44b
Christian Galsterer <christian.galsterer@gmx.de>
2015-11-29 18:38:07 +0100

    - First tests for output events

--- httpevent_test.go
+++ httpevent_test.go
@@ -12,14 +12,24 @@ func TestHttpEventToMapStr(t *testing.T) {
 	fields := make(map[string]string)
 	fields["field1"] = "value1"
 	fields["field2"] = "value2"
+	request := Request{}
+	request.Url = "www.example.org"
+	headers := make(map[string]string)
+	headers["header1"] = "value1"
+	request.Headers = headers
+	request.Body = "Body"
+	request.Method = "get"
 
 	event := HttpEvent{}
 	event.Fields = fields
 	event.DocumentType = "test"
 	event.ReadTime = now
+	event.Request = request
 	mapStr := event.ToMapStr()
 	_, fieldsExist := mapStr["fields"]
 	assert.True(t, fieldsExist)
+	_, requestExist := mapStr["request"]
+	assert.True(t, requestExist)
 	assert.Equal(t, "test", mapStr["type"])
 	assert.Equal(t, common.Time(now), mapStr["@timestamp"])
 }
